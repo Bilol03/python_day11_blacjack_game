5c5f160ac15097510f26ee639c8bcb0952ee4dad
Bilol Madiyarov <bilol@Bilols-MacBook-Air.local>
2023-04-01 03:01:41 +0900

    Dealing cards for user and computer

--- blac_jack.py
+++ blac_jack.py
@@ -6,3 +6,18 @@ def deal_card(card_list):
   return random_cards
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+
+#  deal cards
+
+user_cards = []
+computer_cards = []
+
+deal_number = 2
+
+for deals in range(0, deal_number):
+  user_cards.append(deal_card(cards))
+  computer_cards.append(deal_card(cards))
+
+print("Your cards are: ", user_cards)
+print("Computer's first card is: ", computer_cards[0])
